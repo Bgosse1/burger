0f2a1ae8abb7bb8d7f1d474214be1edb1a7bc4ae
bryant.gossett@gmail.com <5419632+Bgosse1@users.noreply.github.com>
2019-01-09 13:34:59 -0500

    init project structure

---
+++ config/orm.js
@@ -0,0 +1,16 @@
+var connection = require("../config/connection.js");
+
+var orm = {
+    selectAll: function(){
+
+    },
+    insertOne: function (){
+
+    },
+    updateOne: function (){
+
+    }
+  };
+  
+  // Export the orm object for the model (cat.js).
+  module.exports = orm;
